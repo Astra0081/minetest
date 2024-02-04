88759165ee364cd036ef8b6726d9fd1c18afaf49
Gregor Parzefall <gregor.parzefall@posteo.de>
2024-02-04 22:22:03 +0100

    Expose analog joystick input to the Lua API

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -2648,6 +2648,7 @@ void Game::updatePlayerControl(const CameraOrientation &cam)
 		input->getMovementSpeed(),
 		input->getMovementDirection()
 	);
+	control.setMovementFromKeys();
 
 	// autoforward if set: move at maximum speed
 	if (player->getPlayerSettings().continuous_forward &&
