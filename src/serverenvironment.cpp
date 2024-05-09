c3345bec540d5d2465478c3224c99708399b9ddc
Lars Mueller <appgurulars@gmx.de>
2024-05-09 18:19:28 +0200

    hook the thing up

--- src/serverenvironment.cpp
+++ src/serverenvironment.cpp
@@ -2528,3 +2528,8 @@ bool ServerEnvironment::migrateAuthDatabase(
 	}
 	return true;
 }
+
+// HACK
+void ServerEnvironment_updatePos(ServerEnvironment *senv, const v3f &pos, u16 id) {
+	senv->updatePos(pos, id);
+}
